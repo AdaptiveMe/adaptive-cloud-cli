6cd0d5a19194b2ac4afcc23029f1194db9052e07
Ferran Vilà Conesa <ikocel1@gmail.com>
2015-10-23 09:46:59 +0200

    Bug fixing

--- src/adaptive.js
+++ src/adaptive.js
@@ -1,24 +1,24 @@
 #! /usr/bin/env node
 
-const program = require('commander');
-const colors = require('colors');
-const updateNotifier = require('update-notifier');
-const pkg = require('../package.json');
+var program = require('commander');
+var colors = require('colors');
+var updateNotifier = require('update-notifier');
+var pkg = require('../package.json');
 
-const notifier = updateNotifier({pkg});
+var notifier = updateNotifier({pkg: pkg});
 notifier.notify();
 //notifier.notify();
 //console.log(notifier.update);
 
 // Program definition
 /*program
-  .version(pkg.version)
-  .command('login [username]', 'Login into the Adaptive Cloud Platform with your username')
-  .command('register [email]', 'Register user into the Adaptive Cloud Platform')
-  .command('reset-password [email]', 'Reset the password for the user')
-  .command('change-password', 'Change the password for the current user')
-  .command('unregister', 'Remove all the account information for the user')
-  .command('logout', 'Logout from the Adaptive Cloud Platform')
-  .parse(process.argv);*/
+ .version(pkg.version)
+ .command('login [username]', 'Login into the Adaptive Cloud Platform with your username')
+ .command('register [email]', 'Register user into the Adaptive Cloud Platform')
+ .command('reset-password [email]', 'Reset the password for the user')
+ .command('change-password', 'Change the password for the current user')
+ .command('unregister', 'Remove all the account information for the user')
+ .command('logout', 'Logout from the Adaptive Cloud Platform')
+ .parse(process.argv);*/
 
 
